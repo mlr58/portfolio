58109b75f63f1bcf1806535427efdef3bbe2fce3
Manuel Lora Ruiz <166216437+mlr58@users.noreply.github.com>
2026-02-02 23:12:47 +0100

    Optimize fade-in animation with requestAnimationFrame

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 const fadeEls = document.querySelectorAll('.fade-in');
-function inViewport(el){ 
-  const rect = el.getBoundingClientRect(); 
-  return rect.top <= window.innerHeight && rect.bottom >= 0; 
+let ticking = false;
+
+function inViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return rect.top <= window.innerHeight && rect.bottom >= 0;
 }
-function animateFade(){ 
-  fadeEls.forEach(el => { if(inViewport(el)) el.classList.add('show'); }); 
+
+function animateFade() {
+  fadeEls.forEach(el => {
+    if (inViewport(el)) el.classList.add('show');
+  });
 }
-window.addEventListener('scroll', animateFade); 
+
+window.addEventListener('scroll', () => {
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      animateFade();
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
+
 animateFade();
